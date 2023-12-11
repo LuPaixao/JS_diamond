252754b0b32c68034744d95f2b251879c0340d6c
Luana Albuquerque Moreira <luanapaixao.am@gmail.com>
2023-12-11 20:28:50 -0300

    finalizacao

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,6 @@
 function imparPar() {
-  // Obtém o valor do campo
   var num = document.getElementById("num").value;
   
-  // Verifica se o número é par
   if (num % 2 === 0) {
     alert("O número: " + num + " é par.");
   } else {
@@ -11,11 +9,9 @@ function imparPar() {
 }
   
 function maiorNum() {
-  // Obtém os valores dos campos
   var num1 = document.getElementById("num1").value;
   var num2 = document.getElementById("num2").value;
   
-  // Verifica qual é o maior número
   if (num1 > num2) {
     alert("O maior número é " + num1);
   } else if (num2 > num1) {
@@ -26,13 +22,10 @@ function maiorNum() {
 }
 
 function votacao() {
-  // Obtém o ano de nascimento
   var nascimento = document.getElementById("nascimento").value;
 
-  // Calcula a idade da pessoa
   var idade = 2023 - nascimento;
 
-  // Verifica se a pessoa pode votar
   if (idade >= 18) {
     alert("A pessoa pode votar.");
   } else {
@@ -41,37 +34,26 @@ function votacao() {
 }
 
 function salRea() {
-  // Obtém o salário atual
   var salario = parseFloat(document.getElementById("salario").value);
 
-  // Obtém o percentual de reajuste
   var reajuste = parseFloat(document.getElementById("reajuste").value);
 
-  // Calcula o valor do aumento
-  //var aumento = (salario * reajuste) / 100;
-
-  // Calcula o novo salário
   var novoSalario = salario + (salario * reajuste) / 100;
 
-  // Mostra o novo salário
   alert("O novo salário é de R$" + novoSalario);
 }
 
 function semana() {
-  // Obtém o número do dia da semana
   var data = document.getElementById("data").value;
 
-  // Verifica se o número está entre 1 e 7
   if (data < 1 || data > 7) {
     alert("O número deve estar entre 1 e 7.");
     return;
   }
 
-  // Obtém o dia da semana correspondente
   var dia = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
   var diaSemana = dia[data - 1];
 
-  // Mostra o dia da semana
   alert("O dia da semana é " + diaSemana);
 }
 
@@ -88,43 +70,73 @@ function valoresPar(n) {
 }
 
 function somarAnteriores() {
-  // Obtém o número
   var nume = document.getElementById("nume").value;
 
-  // Verifica se o número é positivo
   if (nume % 2 !== 0) {
     alert("O número deve ser par.");
     return;
   }
 
-  // Usa um for loop para iterar sobre os números
   var soma = 0;
   for (var i = 1; i < nume; i++) {
-    // Soma o número atual
     soma += i;
   }
-  // Mostra a soma
   document.getElementById("soma").innerHTML = soma;
 }
 
 function imparAnteriores() {
-  // Obtém o número
   var impar = document.getElementById("impar").value;
 
-  // Verifica se o número é positivo
   if (Math.sign(impar) === -1) {
     alert("O número deve ser positivo.");
     return;
   }
-
-  // Limpa o conteúdo do elemento `numeros`
   document.getElementById("numeros").innerHTML = "";
 
-  // Usa um for loop para iterar sobre os números
   for (var i = 1; i <= impar; i += 2) {
-    // Mostra o número atual
     document.getElementById("numeros").innerHTML += i + " ";
   }
 }
 
-  
+
+function listArray() {
+  const numbers = document.getElementById("numbers").value;
+  const numbersArray = numbers.split("-").map(Number);
+  const sum = numbersArray.filter(number => number < 30).reduce((a, b) => a + b, 0);
+  document.getElementById("resultArray").innerHTML = sum;
+}
+
+
+function arrayMenor() {
+  const numbers = document.getElementById("numbers").value;
+  const count = countNumbers50(numbers);
+  document.getElementById("result").innerHTML = count;
+}
+
+function countNumbers50(numbers) {
+  const numbersArray = numbers.split("/").map(Number);
+  const count = numbersArray.filter(number => number > 50).length;
+  return count;
+}
+
+function inverter() {
+  var string = document.getElementById("string").value;
+  var inverso = "";
+  for (var i = string.length - 1; i >= 0; i--) {
+    inverso += string[i];
+  }
+  document.getElementById("inverso").innerHTML = inverso;
+}
+
+function finalDigito() {
+  const digito = document.getElementById("digito").value;
+  const capitalized = letrasMaiores(digito);
+  document.getElementById("digitoFinal").innerHTML = "A string com a primeira e a última letra maiúsculas é: " + capitalized;
+}
+
+function letrasMaiores(str) {
+  const firstLetter = str.charAt(0).toUpperCase();
+  const lastLetter = str.charAt(str.length - 1).toUpperCase();
+  const middleLetters = str.slice(1, -1);
+  return firstLetter + middleLetters + lastLetter;
+}
